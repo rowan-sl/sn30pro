4daccd6b90a8e5d300bc02f906a994e7350878a1
Rowan S-L <rowan.with.cats@gmail.com>
2022-02-07 16:13:59 -0500

    fix dpad api

--- src/parts/direction_pad.rs
+++ src/parts/direction_pad.rs
@@ -7,11 +7,27 @@ pub struct DirectionPad {
 }
 
 impl DirectionPad {
+    pub fn x(&self) -> &DirectionX {
+        &self.dir_x
+    }
+
+    pub fn y(&self) -> &DirectionY {
+        &self.dir_y
+    }
+
     pub fn up(&self) -> bool {
         self.dir_x == DirectionX::Up
     }
 
     pub fn down(&self) -> bool {
         self.dir_x == DirectionX::Down
+    }
+
+    pub fn left(&self) -> bool {
+        self.dir_y == DirectionY::Left
+    }
+
+    pub fn right(&self) -> bool {
+        self.dir_y == DirectionY::Right
     }
 }
